662924eb3b381eaed7694ea97a266064e2a699ae
abdul wahab <awahab@octavebytes.com>
2025-09-19 18:01:08 +0500

    oath functionality

--- src/component/Install.jsx
+++ src/component/Install.jsx
@@ -3,24 +3,6 @@ import installimgae from "../assets/OB-Cloud-2-1.png"
 
 const Install = () => {
   return (
-//    <div className="h-screen w-full bg-white rounded-2xl">
-//   <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//     <a
-//       href="https://dxkzxrl20d.execute-api.us-east-1.amazonaws.com/dev/install"
-//       className="flex flex-col items-center p-6 bg-white shadow-lg hover:shadow-xl transition duration-300 w-full"
-//     >
-//       <img
-//         src={installimgae}
-//         alt="Octavebytes"
-//         className=" mb-4"
-//       />
-//       <button className="text-xl sm:text-2xl font-semibold text-gray-800 hover:text-blue-600 text-center">
-//         Install the app
-//       </button>
-//     </a>
-//   </div>
-// </div>
-
 <div className="h-screen w-full flex items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
   <div className="bg-white rounded-2xl shadow-lg hover:shadow-xl transition duration-300 p-8 ">
     <a
